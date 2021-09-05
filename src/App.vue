60ca4a2d59178d640363ee4412c519384570155b
Alex <alexj1087@yandex.ru>
2021-09-05 14:59:31 +0300

    become cli version

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,375 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
+    <div class="container">
+      <div class="w-full my-4"></div>
+      <section>
+        <div class="flex">
+          <div class="max-w-xs">
+            <label for="wallet" class="block text-sm font-medium text-gray-700"
+              >Тикер {{ ticker }}</label
+            >
+            <div class="mt-1 relative rounded-md shadow-md">
+              <input
+                @input="input"
+                @keydown.enter="add"
+                v-model="ticker"
+                type="text"
+                name="wallet"
+                id="wallet"
+                class="
+                  block
+                  w-full
+                  pr-10
+                  border-gray-300
+                  text-gray-900
+                  focus:outline-none focus:ring-gray-500 focus:border-gray-500
+                  sm:text-sm
+                  rounded-md
+                "
+                placeholder="Например DOGE"
+              />
+              <div
+                style="
+                  display: flex;
+                  justify-content: space-evenly;
+                  align-items: center;
+                  padding: 2px;
+                "
+              >
+                <button
+                  @click="
+                    ticker = 'BTC';
+                    add();
+                  "
+                  type="button"
+                  style="
+                    background: lightgray;
+                    border-radius: 10px;
+                    margin: 2px;
+                    padding: 2px 15px;
+                  "
+                >
+                  BTC
+                </button>
+                <button
+                  @click="
+                    ticker = 'DOGE';
+                    add();
+                  "
+                  type="button"
+                  style="
+                    background: lightgray;
+                    border-radius: 10px;
+                    margin: 2px;
+                    padding: 2px 15px;
+                  "
+                >
+                  DOGE
+                </button>
+                <button
+                  @click="
+                    ticker = 'ETH';
+                    add();
+                  "
+                  type="button"
+                  style="
+                    background: lightgray;
+                    border-radius: 10px;
+                    margin: 2px;
+                    padding: 2px 15px;
+                  "
+                >
+                  ETH
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button
+          @click="add"
+          type="button"
+          class="
+            my-4
+            inline-flex
+            items-center
+            py-2
+            px-4
+            border border-transparent
+            shadow-sm
+            text-sm
+            leading-4
+            font-medium
+            rounded-full
+            text-white
+            bg-gray-600
+            hover:bg-gray-700
+            transition-colors
+            duration-300
+            focus:outline-none
+            focus:ring-2
+            focus:ring-offset-2
+            focus:ring-gray-500
+          "
+        >
+          <!-- Heroicon name: solid/mail -->
+          <svg
+            class="-ml-0.5 mr-2 h-6 w-6"
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            viewBox="0 0 24 24"
+            fill="#ffffff"
+          >
+            <path
+              d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
+            ></path>
+          </svg>
+          Добавить
+        </button>
+      </section>
+      <template v-if="tickers.length">
+        <hr class="w-full border-t border-gray-600 my-4" />
+        <div>
+          <button
+            v-if="page > 1"
+            @click="page = page - 1"
+            style="
+              background: lightgray;
+              border-radius: 10px;
+              margin: 2px;
+              padding: 2px 15px;
+            "
+          >
+            Назад
+          </button>
+          <button
+            v-if="hasNextPage"
+            @click="page = page + 1"
+            style="
+              background: lightgray;
+              border-radius: 10px;
+              margin: 2px;
+              padding: 2px 15px;
+            "
+          >
+            Вперед
+          </button>
+          <div>Фильтр: <input v-model="filter" type="text" /></div>
+        </div>
+        <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+          <div
+            v-for="t in filteredList()"
+            :key="t.name"
+            @click="select(t)"
+            :class="{
+              'border-4': sel === t,
+            }"
+            class="
+              bg-white
+              overflow-hidden
+              shadow
+              rounded-lg
+              border-purple-800 border-solid
+              cursor-pointer
+            "
+          >
+            <div class="px-4 py-5 sm:p-6 text-center">
+              <dt class="text-sm font-medium text-gray-500 truncate">
+                {{ t.name }} - USD
+              </dt>
+              <dd class="mt-1 text-3xl font-semibold text-gray-900">
+                {{ t.price }}
+              </dd>
+            </div>
+            <div class="w-full border-t border-gray-200"></div>
+            <button
+              @click.stop="handleDelete(t)"
+              class="
+                flex
+                items-center
+                justify-center
+                font-medium
+                w-full
+                bg-gray-100
+                px-4
+                py-4
+                sm:px-6
+                text-md text-gray-500
+                hover:text-gray-600 hover:bg-gray-200 hover:opacity-20
+                transition-all
+                focus:outline-none
+              "
+            >
+              <svg
+                class="h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="#718096"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                  clip-rule="evenodd"
+                ></path></svg
+              >Удалить
+            </button>
+          </div>
+        </dl>
+        <hr class="w-full border-t border-gray-600 my-4" />
+      </template>
+      <section v-if="sel" class="relative">
+        <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
+          {{ sel.name }} - USD
+        </h3>
+        <div
+          class="flex items-end border-gray-600 border-b border-l h-64"
+          style="flex-direction: left"
+        >
+          <div
+            style="position: revert; right: 0"
+            v-for="(col, idx) in normalizeGraph()"
+            :key="idx"
+            :style="{ height: `${col}%` }"
+            class="bg-purple-800 border w-10 h-24"
+          ></div>
+        </div>
+        <button
+          @click="sel = null"
+          type="button"
+          class="absolute top-0 right-0"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            xmlns:svgjs="http://svgjs.com/svgjs"
+            version="1.1"
+            width="30"
+            height="30"
+            x="0"
+            y="0"
+            viewBox="0 0 511.76 511.76"
+            style="enable-background: new 0 0 512 512"
+            xml:space="preserve"
+          >
+            <g>
+              <path
+                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
+                fill="#718096"
+                data-original="#000000"
+              ></path>
+            </g>
+          </svg>
+        </button>
+      </section>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      ticker: null,
+      tickers: [],
+      sel: null,
+      graph: [],
+      page: 1,
+      filter: "",
+      hasNextPage: false,
+    };
+  },
+  created() {
+    const windowData = Object.fromEntries(
+      new URL(window.location).searchParams.entries()
+    );
+
+    if (windowData.filter) this.filter = windowData.filter;
+    if (windowData.page) this.page = +windowData.page;
+
+    const tickersData = localStorage.getItem("cryptonomicon-list");
+    if (tickersData) {
+      this.tickers = JSON.parse(tickersData);
+      this.tickers.forEach((ticker) => {
+        this.subscribeToUpdate(ticker.name);
+      });
+    }
+  },
+  methods: {
+    input() {
+      this.ticker = this.ticker.toUpperCase();
+    },
+    filteredList() {
+      const start = (this.page - 1) * 6;
+      const end = this.page * 6;
+      const filteredTickers = this.tickers.filter((t) =>
+        t.name.includes(this.filter.toUpperCase())
+      );
+      this.hasNextPage = filteredTickers.length > end;
+      return filteredTickers.slice(start, end);
+    },
+    add() {
+      if (!this.ticker) return;
+      if (this.tickers.find((t) => t.name == this.ticker)) {
+        this.ticker = "";
+        return;
+      }
+      const newTicker = {
+        name: this.ticker.toUpperCase(),
+        price: "-",
+      };
+      this.tickers.push(newTicker);
+      localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
+      this.subscribeToUpdate(newTicker.name);
+      this.filter = "";
+    },
+    subscribeToUpdate(tickerName) {
+      setInterval(async () => {
+        const f = await fetch(
+          `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=54c6ccd58c4c27bbb73c44cce886dcfc6adc6c0c73e3c4ab269594bfa205d55e`
+        );
+        const data = await f.json();
+        this.tickers.find((t) => t.name == tickerName).price =
+          data.USD > 1 ? data.USD?.toFixed(2) : data.USD?.toPrecision(2) || "-";
+        if (this.sel?.name == tickerName) this.graph.push(data.USD);
+      }, 5000);
+      this.ticker = "";
+    },
+
+    select(ticker) {
+      (this.sel = ticker), (this.graph = []);
+    },
+
+    handleDelete(currentTicker) {
+      this.tickers = this.tickers.filter((t) => t != currentTicker);
+      if (currentTicker == this.sel) this.sel = null;
+      localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
+    },
+    normalizeGraph() {
+      const maxValue = Math.max(...this.graph);
+      const minValue = Math.min(...this.graph);
+      return this.graph.map(
+        (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
+      );
+    },
+  },
+  watch: {
+    filter() {
+      this.page = 1;
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
+    },
+    page() {
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
+    },
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
